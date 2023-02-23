72f2d4631fb9c7d66171871778a0f22e36ae38ee
Hemstone <hemstone456@gmail.com>
2023-02-23 12:19:28 +0300

    backend users data

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosClient from '../axios-client'
+
+const [users, setUsers]=useState({})
+const [loading, setLoading]=useState(false)
+
+useEffect(()=>{
+  getUsers();
+},[])
+
+const getUsers=()=>{
+  setLoading(true)
+  axiosClient.get('/users')
+  .then(({data})=>{
+    setLoading(false)
+    console.log(data)
+  })
+  .catch(()=>{
+    setLoading(false)
+  })
+}
 
 function Users() {
   return (
